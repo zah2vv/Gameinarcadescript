61e4927f800e6bbe53421a007843ce6559d66770
zah2vv <ant8hony2@gmail.com>
2025-02-19 13:43:27 -0800

    Update zyware.vip.lua

--- zyware.vip.lua
+++ zyware.vip.lua
@@ -5385,7 +5385,7 @@ function library:Load(options)
 
         misc:Toggle{
             name = "Show Player List",
-            default = library.keybind_list_default,
+            default = false,
             flag = "player_list",
             callback = function(value)
                 library.Playerlist.toggled = value
